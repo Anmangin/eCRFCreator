de8eb3761031cad30bb778b83fca7b46e8526014
Thibaut Payen <thibaut.payen@gustaveroussy.fr>
2026-03-04 14:50:54 +0100

    Added the generation of "DVP" style excel and a docx file that looks like a CRF

--- Source/XML_Function.py
+++ Source/XML_Function.py
@@ -1,6 +1,8 @@
 
 import json
 import xml.etree.ElementTree as ET
+import xlsxwriter
+from bs4 import BeautifulSoup
 
 
 
@@ -65,20 +67,11 @@ def lire_et_trier_donnees(pathfileXML, config_path=r'Python\config.json'):
     # Ajouter le champ "Display" pour ProItem
     data["ProItem"] = ajouter_display_pro_item(data)
 
-
-
-
     return data
-
-
 
 
 def ajouter_display_pro_codelist(data):
     """Ajoute un champ 'Display' pour les ProCodeList."""
-
-
-
-
     for key, value in data["ProCodeList"].items():
         # Filtrer les items associés à la ProCodeList actuelle
         filtered_items = {k: v for k, v in data["ProCodeListItem"].items() if v["ProCodeListGuid"] == key}
@@ -137,7 +130,39 @@ def find_arbo(TBNode):
             return source
     
 
+def get_check_type(number):
+    if   number =="1":return "Valid"
+    elif number =="2":return "Required"
+    elif number =="3":return "Enabled"
+    elif number =="5":return "Required Enabled"
+    elif number =="6":return "Hidden"
+    elif number =="7":return "Read only"
+    elif number =="8":return "Caption change"
+    elif number =="9":return "DVC"
+    elif number =="10":return "DVA"
+    elif number =="11":return "Email"
+    elif number =="12":return "Create Form"
+    elif number =="14":return "Post save warning"
+    elif number =="17":return "Unique value required"
+    elif number =="21":return "Disabled Hidden"
+    elif number =="22":return "Dynamic codelist setup"
+    elif number =="23":return "Dynamic codelist filter"
+    return number
 
+def check_data_type(number):
+    if   number =="1":return "Text Box"
+    elif number =="2":return "Text Area"
+    elif number =="3":return "Radio Button"
+    elif number =="4":return "Check Box"
+    elif number =="5":return "Combo Box"
+    elif number =="6":return "Year"
+    elif number =="7":return "YearMonth"
+    elif number =="8":return "Date"
+    elif number =="10":return "Time"
+    elif number =="13":return "Label"
+    elif number =="16":return "Textbox with MedDRA"
+    elif number =="18":return "Timer"
+    return number
 
 
 def get_message(data,ProItemGuid,ProGroupGuid,ProFormGuid):
@@ -149,20 +174,7 @@ def get_message(data,ProItemGuid,ProGroupGuid,ProFormGuid):
         if (Edit["ProItemGuid"] == ProItemGuid) and (Edit["TargetProFormGuid"]==ProFormGuid or Edit["TargetProFormGuid"] is None) and (Edit["TargetProGroupGuid"]==ProGroupGuid or Edit["TargetProGroupGuid"] is None)  :
             i+=1
             EditACtion= Edit["ProEditActionId"]
-            if   EditACtion =="1"           :EditACtion="Valid"
-            elif EditACtion =="2"           :EditACtion="Required"
-            elif EditACtion =="3"           :EditACtion="Enabled"
-            elif EditACtion =="5"           :EditACtion="Required Enabled"
-            elif EditACtion =="6"           :EditACtion="Hidden"
-            elif EditACtion =="7"           :EditACtion="Read only"
-            elif EditACtion =="8"           :EditACtion="Caption change"
-            elif EditACtion =="9"           :EditACtion="DVC"
-            elif EditACtion =="10"          :EditACtion="DVA"
-            elif EditACtion =="11"          :EditACtion="Email"
-            elif EditACtion =="12"          :EditACtion="Create Form"
-            elif EditACtion =="14"          :EditACtion="Post save warning"
-            elif EditACtion =="21"           :EditACtion="Disabled Hidden"
-            elif EditACtion =="23"          :EditACtion="Dynamic codelist filter"
+            EditACtion = get_check_type(EditACtion)
             msg=Edit["Message"]
             chk=Edit["ActionExpression"]
             DTE=Edit['DataExpression']
@@ -267,8 +279,6 @@ def ADD_FORM(data,V_description,ProFormGuid,order,JSON_EXPORT):
 
 
 
-
-
 def get_JSONLIGNE(JSON_EXPORT, V_description, F_description, G_description, GI_key,
                   order,I_description,I_caption, Message, rep,display, I_Status, i_SasName):
     # Assure-toi que la clé 'Patient' existe dans JSON_EXPORT
@@ -312,3 +322,266 @@ def get_JSONLIGNE(JSON_EXPORT, V_description, F_description, G_description, GI_k
     groupe['questions'].append(question)
     
     return JSON_EXPORT
+
+
+def create_graph(data):
+    dictionnary = dict()
+    nodes = ["Trial", "Site", "Patient", "Visit", "Form", "Group", "Item", "CodeList", "CodeListItem"]
+    for nodetype in nodes:
+        name = "Pro" + nodetype
+        for elem in data.iter(name):
+            try:
+                guid = elem.find("ProObjectGuid").text
+                dictionnary[guid] = dict()
+                dictionnary[guid]['child'] = list()
+                dictionnary[guid]['parent'] = list()
+                dictionnary[guid]['Caption'] = elem.find('Caption').text
+                dictionnary[guid]['tag'] = elem.tag
+            except AttributeError:
+                pass
+            try:
+                dictionnary[guid]['SasName'] = elem.find('SasName').text
+            except:
+                pass
+            try:
+                dictionnary[guid]['type'] = elem.find('ProControlTypeId').text
+            except:
+                pass
+            try:
+                if name != "ProCodeListItem":
+                    dictionnary[guid]['child'].append((elem.find('ProCodeListGuid').text, 1))
+            except:
+                pass
+            try:
+                dictionnary[elem.find('ProCodeListGuid').text]['parent'].append(guid)
+            except:
+                pass
+            try:
+                dictionnary[guid]['Description'] = elem.find('Description').text
+            except:
+                pass
+    # Links
+    for i in range(6):
+        link = "Pro" + nodes[i] + nodes[i+1]
+        for elem in data.iter(link):
+            parent = elem.find("Pro" + nodes[i]  + "Guid").text
+            child =  elem.find("Pro" + nodes[i+1]+ "Guid").text
+            order = int(elem.find("OrderNo").text)
+            dictionnary[parent]['child'].append((child, order))
+            dictionnary[child]['parent'].append(parent)
+            try:
+                dictionnary[child]['MaxOccurance'] = elem.find('MaxOccurance').text
+            except:
+                pass
+    for elem in data.iter("ProCodeListItem"):
+        parent = elem.find("ProCodeListGuid").text
+        child = elem.find("ProObjectGuid").text
+        order = int(elem.find("OrderNo").text)
+        dictionnary[parent]['child'].append((child, order))
+        dictionnary[child]['parent'].append(parent)
+        dictionnary[guid]['Caption'] = elem.find('Caption').text
+        dictionnary[guid]['Value'] = elem.find('Value').text
+    return dictionnary
+
+def create_edit_check_dictionnary(data):
+    dictionnary = dict()
+    keys = ["OID", "ProEditActionId", "TargetPath", "ActionExpression", "DataExpression"]
+    for proedit in data.iter("ProEdit"):
+        guids = list()
+        try:
+            guids.append(proedit.find("ProItemGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProGroupGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProFormGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProVisitGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProPatientGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProSiteGuid").text)
+        except AttributeError:
+             pass
+        try:
+            guids.append(proedit.find("ProTrialGuid").text)
+        except:
+             pass
+        OID = proedit.find('OID').text
+        dictionnary[OID] = dict()
+        if len(guids) == 0: print(OID)
+        for k in keys:
+            sub = proedit.find(k).text
+            dictionnary[OID][k] = sub
+        dictionnary[OID]["lower"] = guids[0]
+    return dictionnary
+
+# Useless, does not function with return
+def recursive_move(graph, elem):
+    try:
+        print (graph[elem]['Caption'], graph[elem]['SasName'], check_data_type(graph[elem]['type']))
+    except:
+        loc = graph[elem]['child']
+        for e in loc:
+            recursive_move(graph, e)
+
+def print_xls_from_edit_check(xmlFile, xlsName):
+    data = ET.parse(xmlFile)
+    editChecks = create_edit_check_dictionnary(data)
+    graph = create_graph(data)
+    # Create an new Excel file and add a worksheet
+    workbook = xlsxwriter.Workbook(xlsName)
+    worksheet = workbook.add_worksheet("Checks")
+    cell_format = workbook.add_format()
+    cell_format.set_text_wrap()
+    ROWS = ['Item Name', "Item Type", "Test Type", "Action", "Data"]
+    # TITLE ROW
+    for i in range(len(ROWS)):
+         worksheet.write(0, i, ROWS[i])
+    l = 1
+    checked = list()
+    for k, v in editChecks.items():
+        action = v['ActionExpression']
+        # Remove the empty lines
+        try:
+            action = "\n".join([ll.rstrip() for ll in action.splitlines() if ll.strip()])
+        except AttributeError:
+             action = ""
+        data = v['DataExpression']
+        # Remove the empty lines
+        try:
+            data = "\n".join([ll.rstrip() for ll in data.splitlines() if ll.strip()])
+        except AttributeError:
+            data = ""           
+        item = graph[editChecks[k]['lower']]
+        checked.append(editChecks[k]['lower'])
+        worksheet.write(l, 0, item['Caption'])
+        worksheet.write(l, 1,item['tag'])
+        worksheet.write(l, 2, get_check_type(v['ProEditActionId']))
+        worksheet.write(l, 3, action, cell_format)
+        worksheet.write(l, 4, data, cell_format)
+        l += 1
+    worksheet.autofit()
+    worksheet.autofilter(0, 0, l - 1, len(ROWS)-1)
+    worksheet2 = workbook.add_worksheet('Items Without Checks')
+    l = 1
+    worksheet2.write(0, 0, 'Caption', cell_format)
+    worksheet2.write(0, 1, 'SasName', cell_format)
+    worksheet2.write(0, 2, 'type', cell_format)
+    for id, node in graph.items():
+        if node['tag'] == "ProItem":
+            if id not in checked:
+                worksheet2.write(l, 0, node['Caption'], cell_format)
+                worksheet2.write(l, 1, node['SasName'], cell_format)
+                worksheet2.write(l, 2, check_data_type(node['type']), cell_format)
+                l += 1
+    worksheet2.autofilter(0, 0, l - 1, 2)
+    worksheet2.autofit()
+    workbook.close()
+
+
+def print_graph(graph, head, lvl=0, fileName=None):
+    lns = graph[head]['child']
+    tag = graph[head]['tag']
+    try:
+        desc = graph[head]['Description'].strip()
+        try:
+            typ = check_data_type(graph[head]['type'])
+        except:
+            typ = ''
+        try:
+            sasNam = graph[head]['SasName']
+        except:
+            sasNam = ''
+        if len(desc) > 0:
+            print(desc, typ, sep="\t", file=fileName)
+    except:
+        print("\t", graph[head]['Caption'], file=fileName)
+    for e in sorted(lns, key = lambda e:e[1]):
+        lvl = lvl + 1
+        print_graph(graph, e[0], lvl, fileName)
+
+
+def print_doc_xml(xmlFile, docFile, head=None):
+    data = ET.parse(xmlFile)
+    graph = create_graph(data)
+    # Find the head
+    if head == None:
+        for k, v in graph.items():
+            if v['tag'] == 'ProTrial':
+                head = k
+                break
+    from docx import Document
+    document = Document()
+    internal_func_doc(graph, document, head=head, lvl=0, buffer=list())
+    document.save(docFile)
+
+
+def internal_func_doc(graph, documentName, head=None, lvl=0, buffer=list(), maxi=False):
+    lns = graph[head]['child']
+    tag = graph[head]['tag']
+    try:
+        desc = graph[head]['Description'].strip()
+        soup = BeautifulSoup(desc, "html.parser")
+        desc = soup.get_text()
+        try:
+            typ = check_data_type(graph[head]['type'])
+        except:
+            typ = ''
+        try:
+            sasNam = graph[head]['SasName']
+        except:
+            sasNam = ''
+        try:
+            repeat = graph[head]['MaxOccurance']
+            if int(repeat) > 1:
+                desc += " ⟳"
+        except:
+            repeat = ""
+        if len(desc) > 0:
+            #print(desc, typ, sep="\t")
+            if len(typ) == 0:
+                try:
+                    documentName.add_heading(desc, level=lvl)
+                except ValueError:
+                    documentName.add_heading(desc, level=9)
+            else:
+                buffer = (desc, typ, list())
+    except:
+        #print("\t", graph[head]['Caption'])
+        buffer[2].append(graph[head]['Caption'])
+    if len(buffer) > 0 and len(lns) == 0 and (maxi or tag != 'ProCodeListItem'):
+        table = documentName.add_table(rows=1, cols=3)
+        hdr_cells = table.rows[0].cells
+        hdr_cells[0].text = buffer[0].replace("\\r\\n", "\n")
+        hdr_cells[1].text = buffer[1]
+        codeList = "\n".join(buffer[2])
+        if len(buffer[2]) > 10:
+            codeList = "\n".join(buffer[2][0:4] + ['...'] + buffer[2][-4:-1])
+        hdr_cells[2].text = codeList
+        buffer = list()
+    ls = sorted(lns, key = lambda e:e[1])
+    lvl = lvl + 1
+    for i in range(len(ls)):
+        e = ls[i]
+        if i == len(ls) - 1:
+            maxi = True
+        else:
+            maxi = False
+        internal_func_doc(graph, documentName, e[0], lvl, buffer, maxi)
+
+
+
+def find_parentals(graph, id):
+    for e in graph[id]['parents']:
+        print(e)
+        find_parentals(graph, e)
